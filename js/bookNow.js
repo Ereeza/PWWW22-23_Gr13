c761913b625b99cfa5a05a0275dc1c683a53fdbf
Fjolla12 <=>
2023-01-04 13:49:10 +0100

    Added card page

--- js/bookNow.js
+++ js/bookNow.js
@@ -3,7 +3,7 @@ const numOfPersons = parseInt(prompt("Enter number of persons"));
 const withFood = parseInt(prompt("With or without food (1 or 0)"));
 
 for (let item of packages) {
-    outputPackagesRow(item);
+  outputPackagesRow(item);
 }
 
 const total = calculateTotal(packageIndex, numOfPersons, withFood);
